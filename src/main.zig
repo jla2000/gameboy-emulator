f3afb995fa97f53dbe2d3e1b4124290fd250ec71
Lafferton, Jan <jan.lafferton@vector.com>
2025-09-29 16:52:59 +0200

    Simplify

--- src/main.zig
+++ src/main.zig
@@ -12,48 +12,12 @@ const Flags = enum {
     Z, N, H, C,
 };
 
-fn gen_opcode(comptime opcode: u8) OpcodeFn {
-    switch (opcode) {
-        0x00 => return struct {
-            pub fn exec() void {
-                std.debug.print("NOP\n", .{});
-            }
-        }.exec,
-        else => return struct {
-            pub fn exec() void {
-                std.debug.print("Unimplemented opcode: 0x{x}\n", .{opcode});
-            }
-        }.exec,
-    }
-}
-
-const OpcodeFn = *const fn() void;
-const OpcodeTable = [256]OpcodeFn;
-
-fn gen_opcode_table() OpcodeTable {
-    comptime var table: OpcodeTable = undefined;
-
-    comptime for (0..table.len) |opcode| {
-        table[opcode] = gen_opcode(opcode);
-    };
-
-    return table;
-}
-
 pub fn main() !void {
-    const opcode_table = gen_opcode_table();
-
     const regs = std.mem.zeroes([8]u8);
     const mem = std.mem.zeroes([0x1000]u8);
     var pc: u16 = 0;
 
-    const opcode = mem[pc];
-    opcode_table[opcode]();
-    pc += 1;
-
-    _ = regs;
-
-    rl.InitWindow(800, 600, "gb_emulator");
+    rl.InitWindow(800, 600, "gameboy-emulator");
     defer rl.CloseWindow();
 
     rl.SetTargetFPS(60);

67433f4ddd79bc3af7dfc4c769a82a28f056dd02
Lafferton, Jan <jan.lafferton@vector.com>
2025-09-29 16:42:27 +0200

    Minimal stuff

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,67 @@
 const std = @import("std");
 
-pub fn main() !void {}
+const rl = @cImport({
+    @cInclude("raylib.h");
+});
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+const Register = enum {
+    A, B, C, D, E, F, H, L,
+};
+
+const Flags = enum {
+    Z, N, H, C,
+};
+
+fn gen_opcode(comptime opcode: u8) OpcodeFn {
+    switch (opcode) {
+        0x00 => return struct {
+            pub fn exec() void {
+                std.debug.print("NOP\n", .{});
+            }
+        }.exec,
+        else => return struct {
+            pub fn exec() void {
+                std.debug.print("Unimplemented opcode: 0x{x}\n", .{opcode});
+            }
+        }.exec,
+    }
+}
+
+const OpcodeFn = *const fn() void;
+const OpcodeTable = [256]OpcodeFn;
+
+fn gen_opcode_table() OpcodeTable {
+    comptime var table: OpcodeTable = undefined;
+
+    comptime for (0..table.len) |opcode| {
+        table[opcode] = gen_opcode(opcode);
+    };
+
+    return table;
+}
+
+pub fn main() !void {
+    const opcode_table = gen_opcode_table();
+
+    const regs = std.mem.zeroes([8]u8);
+    const mem = std.mem.zeroes([0x1000]u8);
+    var pc: u16 = 0;
+
+    const opcode = mem[pc];
+    opcode_table[opcode]();
+    pc += 1;
+
+    _ = regs;
+
+    rl.InitWindow(800, 600, "gb_emulator");
+    defer rl.CloseWindow();
+
+    rl.SetTargetFPS(60);
+
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        rl.ClearBackground(rl.RAYWHITE);
+        rl.DrawFPS(0, 0);
+        rl.EndDrawing();
+    }
 }

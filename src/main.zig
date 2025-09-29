6a4aecc14334c437ac07e3cda11416387e5b8410
Lafferton, Jan <jan.lafferton@vector.com>
2025-09-29 17:18:02 +0200

    Remove shaise

--- src/main.zig
+++ src/main.zig
@@ -4,31 +4,14 @@ const rl = @cImport({
     @cInclude("raylib.h");
 });
 
-const Register = enum {
-    A, B, C, D, E, F, H, L,
-};
-
-const Flags = enum {
-    Z, N, H, C,
-};
-
 pub fn main() !void {
-    const regs = std.mem.zeroes([8]u8);
-    const mem = std.mem.zeroes([0x1000]u8);
-    const pc: u16 = 0;
-
-    _ = regs;
-    _ = mem;
-    _ = pc;
-
     rl.InitWindow(800, 600, "gameboy-emulator");
     defer rl.CloseWindow();
 
     rl.SetTargetFPS(60);
-
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
-        rl.ClearBackground(rl.RAYWHITE);
+        rl.ClearBackground(rl.BLACK);
         rl.DrawFPS(0, 0);
         rl.EndDrawing();
     }

33386ebdb40a71790bd372882b4dcf29b707600d
Lafferton, Jan <jan.lafferton@vector.com>
2025-09-29 18:01:55 +0200

    Simple opcdoe

--- src/gameboy.zig
+++ src/gameboy.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+var regs = std.mem.zeroes([8]u8);
 var mem = std.mem.zeroes([1024]u8);
 var pc: u16 = 0;
 var sp: u16 = 0;
@@ -8,8 +9,12 @@ pub fn step() void {
     const opcode = mem[pc];
 
     switch (mem[pc]) {
+        0x00 => {}, // NOP
+        0x33 => {
+            sp += 1;
+        },
         else => {
-            std.debug.print("Unimplemented opcode: 0x{x}", .{opcode});
+            std.debug.panic("Unimplemented opcode: 0x{x}", .{opcode});
         },
     }
 

db47198f32439cc95a9ebde43915afcdd8027232
Lafferton, Jan <jan.lafferton@vector.com>
2025-09-29 16:54:19 +0200

    Fixes

--- src/main.zig
+++ src/main.zig
@@ -15,7 +15,11 @@ const Flags = enum {
 pub fn main() !void {
     const regs = std.mem.zeroes([8]u8);
     const mem = std.mem.zeroes([0x1000]u8);
-    var pc: u16 = 0;
+    const pc: u16 = 0;
+
+    _ = regs;
+    _ = mem;
+    _ = pc;
 
     rl.InitWindow(800, 600, "gameboy-emulator");
     defer rl.CloseWindow();

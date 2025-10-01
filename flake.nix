ce54d7486e027feb5efb7fb984c28c4f5f13e2bc
Lafferton, Jan <jan.lafferton@vector.com>
2025-10-01 17:16:27 +0200

    Cleanup flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { ... }@inputs:
+  outputs = inputs:
     let
-      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [ raylib ];
         nativeBuildInputs = with pkgs; [ zig ];
       };
